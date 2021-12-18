366c849f306130038b8bb93928dcde27798bf957
yash10019coder <lit2020066@iiitl.ac.in>
2021-12-18 17:06:13 +0530

    nit fixes.

--- data/src/test/java/com/iiitlucknow/data/ExampleUnitTest.kt
+++ data/src/test/java/com/iiitlucknow/data/ExampleUnitTest.kt
@@ -1,6 +1,6 @@
 package com.iiitlucknow.data
 
-import org.junit.Assert.*
+import org.junit.Assert.assertEquals
 import org.junit.Test
 
 /**
--- utility/src/test/java/com/iiitlucknow/utility/ExampleUnitTest.kt
+++ utility/src/test/java/com/iiitlucknow/utility/ExampleUnitTest.kt
@@ -1,6 +1,6 @@
 package com.iiitlucknow.utility
 
-import org.junit.Assert.*
+import org.junit.Assert.assertEquals
 import org.junit.Test
 
 /**
