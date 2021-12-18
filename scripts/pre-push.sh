7228c35263fa9dc6d5395bfdf1ea3fd1b7e0c9b6
yash10019coder <lit2020066@iiitl.ac.in>
2021-12-18 18:11:50 +0530

    added protobuf linter.

--- scripts/pre-push.sh
+++ scripts/pre-push.sh
@@ -6,12 +6,11 @@
 # - buf
 # - (others in the future)
 
-if bash scripts/ktlint_lint_check.sh && bash scripts/checkstyle_lint_check.sh ; then
+if bash scripts/ktlint_lint_check.sh && bash scripts/checkstyle_lint_check.sh && bash scripts/buf_lint_check.sh ; then
   echo "All checks passed successfully"
   exit 0
 else
   exit 1
 fi
 
-# TODO(#3000): Add Bazel Linter to the project
 # TODO(#970): Add XML Linter to the project
