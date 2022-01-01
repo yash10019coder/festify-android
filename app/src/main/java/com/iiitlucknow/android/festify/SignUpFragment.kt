2b58c892d40d1335da202a9a1ca04e7945685812
Rohit1173 <lcs2020048@iiitl.ac.in>
2022-01-01 11:09:12 +0530

    added crop option

--- app/src/main/java/com/iiitlucknow/android/festify/SignUpFragment.kt
+++ app/src/main/java/com/iiitlucknow/android/festify/SignUpFragment.kt
@@ -2,6 +2,7 @@ package com.iiitlucknow.android.festify
 
 import android.app.Activity
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -11,10 +12,12 @@ import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import coil.load
 import com.iiitlucknow.android.festify.databinding.FragmentSignUpBinding
+import com.theartofdev.edmodo.cropper.CropImage
+import com.theartofdev.edmodo.cropper.CropImageView
 
 
 class SignUpFragment : Fragment() {
-    private var my_data: String? = null
+    private var my_data: Uri? = null
     private val IMAGE_PICK_CODE = 1000
     private var _binding: FragmentSignUpBinding? = null
     private val binding get() = _binding!!
@@ -32,7 +35,7 @@ class SignUpFragment : Fragment() {
         binding.fSignUpBtn.setOnClickListener {
             if (binding.setUsername.text.toString().trim().isNotEmpty() && my_data != null) {
                 val intent = Intent(activity, MainActivity::class.java)
-                intent.putExtra("p_img", my_data)
+                intent.putExtra("p_img", my_data.toString())
                 intent.putExtra("u_name", binding.setUsername.text.toString().trim())
                 startActivity(intent)
             } else {
@@ -52,9 +55,28 @@ class SignUpFragment : Fragment() {
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
-        if (resultCode == Activity.RESULT_OK && requestCode == IMAGE_PICK_CODE) {
-            my_data = data?.dataString.toString()
-            binding.selProfileImg.load(my_data)
+        if (resultCode == Activity.RESULT_OK )  {
+            when(requestCode) {
+                IMAGE_PICK_CODE -> {
+                    data?.data?.let {
+                        crop_the_image(it)
+                    }
+                }
+                CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE ->{
+                    val result =CropImage.getActivityResult(data)
+                    binding.selProfileImg.load(result.uri)
+                    my_data=result.uri
+            }
+            }
         }
     }
+
+    private fun crop_the_image(uri: Uri) {
+      CropImage.activity(uri)
+          .setGuidelines(CropImageView.Guidelines.ON)
+          .setAspectRatio(1080,1080)
+          .setCropShape(CropImageView.CropShape.OVAL)
+          .start(requireContext(),this)
+    }
+
 }
